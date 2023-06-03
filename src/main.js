0927f139ce59fce48165f36ae8cefb7477056813
prosper edafe <mamusedafe19@gmail.com>
2023-06-03 20:26:39 +0100

    uninstall

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import './assets/index.css';
 import App from './App.vue';
-// import router from './router';
+import router from './router';
 import { createApp } from 'vue';
-import Vue3SmoothScroll from 'vue3-smooth-scroll';
 
-const app = createApp(App)
-
-app.use(Vue3SmoothScroll).mount('#app')
+createApp(App).use(router).mount('#app')
