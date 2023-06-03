26b59fb6ffda854074e63b8ae1391a070f995016
prosper edafe <mamusedafe19@gmail.com>
2023-06-03 20:24:20 +0100

    added smooth scroll

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './assets/index.css';
 import App from './App.vue';
-import router from './router';
+// import router from './router';
 import { createApp } from 'vue';
+import Vue3SmoothScroll from 'vue3-smooth-scroll';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(Vue3SmoothScroll).mount('#app')
