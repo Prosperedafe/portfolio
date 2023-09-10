36568e822466efcb7ee3203c6079dcfa811e18cd
prosper edafe <mamusedafe19@gmail.com>
2023-09-11 00:01:46 +0100

    created new template

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
-<template>
-  <Heading />
-  <Home />
-</template>
-
-<script>
-import Heading from "./components/Header.vue";
-import Home from "./views/Home.vue";
-
-export default {
-  name: "App",
-  components: {
-    Heading,
-    Home,
-  },
-};
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style></style>
+<template></template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
